8dd3102d7a900c693c2078c354267ff7df90e0cf
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-02 06:13:45 -0700

    commt now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from "react";
 import ProductForm from "./pages/ProductForm";
 import ProductShow from "./pages/ProductShow";
 import ProductDetails from "./pages/ProductDetails";
-import SingleProduct from './pages/SingleProduct';
+import SingleProduct from "./pages/SingleProduct";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
- 
   const [product, setProduct] = useState([]);
   axios.defaults.withCredentials = true;
 
   const fetchProducts = async () => {
     try {
-      const resp = await axios.get("https://bacdkend-api.vercel.app/api/getproducts");
+      const resp = await axios.get(
+        "https://bacdkend-api.vercel.app/api/getproducts"
+      );
       setProduct(resp.data);
     } catch (error) {
       console.log("resp err : ", error.message);
@@ -28,12 +28,11 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/ProductDetails" element={<ProductDetails />} />
+        <Route path="/ProductDetails" element={<ProductDetails />} />
         <Route path="/SingleProduct/:productId" element={<SingleProduct />} />
-    
-       
-        <Route 
-          path="/" 
+
+        <Route
+          path="/"
           element={
             <>
               <ProductForm fetchProducts={fetchProducts} />
@@ -47,7 +46,7 @@ function App() {
                 />
               ))}
             </>
-          } 
+          }
         />
       </Routes>
     </Router>
