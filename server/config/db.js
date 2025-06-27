a1c3ccc2488c7eab65d49885947009271e7df985
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2025-06-27 19:04:59 +0500

    cahnge db

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose  = require("mongoose")
 
  exports.connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.DB_URI);
       console.log('✅ MongoDB connected');
     } catch (error) {
       console.error('❌ MongoDB connection error:', error);
